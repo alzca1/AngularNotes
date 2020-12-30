17b9c83e39cefa16449e0d870a0da7e919542ef6
Álvaro Zájara <alzca1@gmail.com>
2020-12-30 13:16:54 +0100

    added search to navbar without implementing search capabilities yet

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.scss'],
+  animations: [
+    trigger('placeholder', [
+      state(
+        'normal',
+        style({
+          transform: 'translateX(0) scale(1,1)',
+          fontSize: '1.2rem',
+          position: 'absolute',
+          top: '20px',
+          left: '70px',
+          opacity: '1',
+          color: '#a9a9a9',
+        })
+      ),
+      state(
+        'selected',
+        style({
+          display: 'block',
+          opacity: 1,
+          transform: 'translate(-20px, -20px) scale(0.5, 0.5)',
+        })
+      ),
+      transition('normal <=> selected', animate(300)),
+    ]),
+  ],
 })
 export class SearchBarComponent implements OnInit {
   @Output() searchValueEmitted = new EventEmitter();
   @Output() toggleSearchBar = new EventEmitter();
-
+  state = 'normal';
   value: string;
 
   constructor() {}
@@ -31,5 +63,14 @@ export class SearchBarComponent implements OnInit {
 
   toggleSearch() {
     this.toggleSearchBar.emit();
+  }
+
+  toggleAnimation() {
+    if (this.value && this.state === 'selected') {
+      return;
+    }
+    this.state === 'normal'
+      ? (this.state = 'selected')
+      : (this.state = 'normal');
   }
 }
